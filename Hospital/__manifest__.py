1fbfb13f2bb1327b6a11c5bd489b209a9452365a
ermias576 <ermiastegegn576@gmail.com>
2023-01-11 00:21:25 +0300

    hospital module commit

---
+++ Hospital/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'R&D Hospital',
+    'version': '14.0.0.0.1',
+    'summary': 'Hospital Managment System for odoo 14',
+    'sequence': -100,
+    'description': """R&D Group Hospital Managment System""",
+    'category': 'Services',
+    'author': 'Ermias Tegegn',
+    'maintainer': 'Ermias Tegegn',
+    'website': 'https://www.randdethiopia.com',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
