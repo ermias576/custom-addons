dde8e9038d262f4ec7de52ca2695e4a872646c83
ermias576 <ermiastegegn576@gmail.com>
2023-01-11 00:59:09 +0300

    hospital module add fields commit

---
+++ Hospital/models/patient.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+
+    name = fields.Char(string='Name', required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('male', 'Male'),
+        ('other', 'other')
+    ], required=True, default = 'male')
+
+    note= fields.Text(String='Description')
