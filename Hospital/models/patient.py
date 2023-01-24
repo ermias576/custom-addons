711b1555b2a18baedd52b59fc01ac8f1a3418853
ermias576 <ermiastegegn576@gmail.com>
2023-01-25 00:27:13 +0300

    many2one and field traker commit

--- Hospital/models/patient.py
+++ Hospital/models/patient.py
@@ -6,19 +6,19 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
 
-    name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
+    name = fields.Char(string='Name', required=True, tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'other')
-    ], required=True, default = 'male')
+    ], required=True, default = 'male', tracking=True)
 
-    note= fields.Text(String='Description')
+    note= fields.Text(String='Description', tracking=True)
     state= fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'),
-                             ('done', 'Done'), ('cancel', 'Cancelled')], default='draft', string="Status")
+                             ('done', 'Done'), ('cancel', 'Cancelled')], default='draft', string="Status", tracking=True)
 
-
+    responsible_id = fields.Many2one('res.partner', string="Responsible")
 
     def action_confirm(self):
         self.state = 'confirm'
